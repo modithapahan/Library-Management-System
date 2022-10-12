6beb1794e5be457ce5848b5c9f4497d5c49e6ccb
modithapahan <modithapahan0@gmail.com>
2022-10-12 20:55:28 +0530

    Backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, ()=> {
+    console.log(`Server is up & running on PORT ${PORT}`);
+});
